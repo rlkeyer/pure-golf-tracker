f0c85c47e8d91cc8551e9301b365961715983504
Rob <rlkeyer@gmail.com>
2024-06-06 00:17:47 -0400

    adding mui and sample chip

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Counter } from "./components/Counter";
+import { Chip } from "@mui/material";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Chip label="In Progress" className="club-chip" />
       <Counter />
     </div>
   );
